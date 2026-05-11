f9b3fe2efdc97ef9e9a042e896542984a09712c5
Minty <Mintyshakespaw@gmail..com>
2026-05-11 14:10:16 +0700

    update code

--- UI/js/bug-report.js
+++ UI/js/bug-report.js
@@ -30,4 +30,19 @@ function closeExportMenu() {
 
 document.addEventListener("click", function () {
   closeExportMenu();
+  closeUserMenu();
 });
+
+function toggleUserMenu(e) {
+  e.stopPropagation();
+  document.getElementById("userMenu").classList.toggle("open");
+}
+
+function closeUserMenu() {
+  const menu = document.getElementById("userMenu");
+  if (menu) menu.classList.remove("open");
+}
+
+function handleLogout() {
+  globalThis.location.href = "login.html";
+}
